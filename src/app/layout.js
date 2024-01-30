b3c6a1240ee2c65e97a2d8ab4a70f8fc7d165ccd
zubair7272 <64643003+zubair7272@users.noreply.github.com>
2024-01-31 02:10:52 +0530

    Default Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl mx-auto  p-4">
+        <main className="">
         <Header />
         {children}
         <Footer/>
