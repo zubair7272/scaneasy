da314d696b97c633cce353dc734ce6ee350296c0
Mohammed Zubair <nisarzubair414@gmail.com>
2024-01-30 18:18:32 +0000

    Update

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -2,12 +2,20 @@ export default function Register(){
     return(
         <section className="mt-9">
 
-            <h1 className="text-center text-red-500 text-4xl">Register</h1>
-            <form action="">
+            <h1 className="text-center text-red-500 text-4xl mb-4">Register</h1>
+            <form action="" className="block max-w-xs mx-auto">
                 <input type="email" placeholder="Email" />
                 <input type="password" placeholder="Password" />
                 <button type="submit">Register</button>
             </form>
+            <div>
+                Or Login 
+            </div>
+            <div>
+                <button>
+                    Login With Google
+                </button>
+            </div>
         </section>
     );
 }
