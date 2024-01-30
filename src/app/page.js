7476e81681153e8f62e56863f415b55b1c1adb91
Mohammed Zubair <nisarzubair414@gmail.com>
2024-01-30 17:07:32 +0000

    fixed contact page error

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+// import ContactForm from "./components/layouts/ContactForm";
 import Header  from "./components/layouts/Header";
 import Hero from "./components/layouts/Hero";
 // import Menu from "./components/layouts/Menu";
@@ -7,9 +8,9 @@ export default function Home() {
     <>
       {/* <Header /> */}
       <Hero />
+      <h1>hi</h1>
       {/* <Items /> */}
       {/* <Menu /> */}
-      
     </>
   )
 }
