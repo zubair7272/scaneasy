9aa4545a0739a27d7cd432db84cfc809a5a5f5da
Mohammed Zubair <nisarzubair414@gmail.com>
2024-01-31 18:52:28 +0000

    Email Password Fetch Implementation

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -1,13 +1,26 @@
+"use client";
 // import next from "next";
 import Image from "next/image"
+import { useState } from "react";
+// import { json } from "stream/consumers";
 export default function Register(){
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    function handleFormSubmit(ev){
+        ev.preventDefault()
+        fetch('/api/register',{
+            method: 'POST',
+            body : JSON.stringify({email,password}),
+            headers : {'Content-Type': 'application/json'}
+        })
+    }
     return(
         <section className="mt-9">
 
             <h1 className="text-center text-red-500 text-4xl mb-4">Register</h1>
-            <form action="" className="block max-w-xs mx-auto">
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+            <form action="" className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+                <input type="email" placeholder="Email" value={email} onChange={ev => setEmail(ev.target.value)}/>
+                <input type="password" placeholder="Password" value={password} onChange={ev => setPassword(ev.target.value)} />
                 <button type="submit">Register</button>
             <div className="my-4 text-center text-gray-600">
                 Or Login 
