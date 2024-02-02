37590c387bcf5e6fb6250a5615952655f5060a4b
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-02 19:42:46 +0000

    Mongo DB Error

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,5 +1,10 @@
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
+import { user } from "../../models/User";
 
-export async function POST(request) {
-    return NextResponse.json({ message: "Hello World" });
+export async function POST(req) {
+  const body = await req.json();
+  mongoose.connect('process.env.MONGO_URL');
+  const Cuser = await user.create()
+    return NextResponse.json({Cuser});
   }
