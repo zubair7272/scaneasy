c7b134535d932887be0e090f37b645a5cd63f52c
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-03 18:47:09 +0000

    compiler error handled

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,10 +4,8 @@ import { User } from "../../models/User";
 
 export async function POST(req) {
   const body = await req.json();
-  mongoose.connect(process.env.MONGO_URL,
-    (err) => {
-    if(err) console.log(err) 
-    else console.log("mongdb is connected");});
+  mongoose.connect(process.env.MONGO_URL).then(()=> console.log('connected Succesfully'))
+  .catch((err)=>{ console.log(err)});
   const cuser = await User.create(body)
     return NextResponse.json(cuser);
   }
