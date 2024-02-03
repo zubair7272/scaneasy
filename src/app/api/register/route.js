d4da3cace310e0264dbfebbf4253762e7328dd0e
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-03 17:57:38 +0000

    eror

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,7 +4,10 @@ import { User } from "../../models/User";
 
 export async function POST(req) {
   const body = await req.json();
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect(process.env.MONGO_URL,
+    (err) => {
+    if(err) console.log(err) 
+    else console.log("mongdb is connected");});
   const cuser = await User.create(body)
     return NextResponse.json(cuser);
   }
