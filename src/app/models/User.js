6c3f306e56dd483967dfa301aa61afa348604ec7
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-03 21:40:01 +0000

    Password Encryption Added

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,14 +1,24 @@
 import { Schema, model, models } from "mongoose";
 // import { type } from "os";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema ({
     email : {type : String,required:true,unique:true},
     password : {type : String,required:true, 
-        // validate: pass=>{
-        //     if (!pass?.length || pass.length<5){
-        //         new Error('Password Is Too Weak')
-            // }
+        validate: pass=>{
+            if (!pass?.length || pass.length<5){
+                new Error('Password Is Too Weak')
+                return false;
+            }
     },
- },{timestamps: true});
+ },
+},{timestamps: true});
+
+UserSchema.post('validate',function(user){
+    const myPlaintextPassword = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(myPlaintextPassword, salt);
+
+})
 
 export const User = models?.User || model('User',UserSchema)
