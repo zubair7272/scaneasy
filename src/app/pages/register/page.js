6d112c05a1abf94b11366da83c686dd2989d467a
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-04 13:14:13 +0000

    Addeed User Registration Conditions

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -2,27 +2,52 @@
 //hello
 // import next from "next";
 import Image from "next/image"
+import Link from "next/link"
 import { useState } from "react";
 // import { json } from "stream/consumers";
 export default function Register(){
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
-    function handleFormSubmit(ev){
+    const [CreatingUser,setCreatingUser] = useState(false);
+    const [UserCreated,setUserCreated] = useState(false);
+    const [Error,setError] = useState(false);
+    async function handleFormSubmit(ev){
         ev.preventDefault()
-        fetch('/api/register',{
+        setCreatingUser(true);
+        setError(false);
+        setUserCreated(false)
+        const response = await fetch('/api/register',{
             method: 'POST',
             body : JSON.stringify({email,password}),
             headers : {'Content-Type': 'application/json'}
         })
+        if(response.ok){
+            setUserCreated(true);
+        }
+        else{
+            setError(true);
+        }
+        setCreatingUser(false);
+        // setUserCreated(true);=   
     }
     return(
         <section className="mt-9">
 
             <h1 className="text-center text-red-500 text-4xl mb-4">Register</h1>
+            {UserCreated &&(
+                <div className="my-4 text-center">
+                    User Created Succesfully!<br></br> You Can <Link href={'/login'}>Login Now &raquo;</Link>
+                </div>
+            )}
+            {Error &&(
+                <div className="my-4 text-center">
+                    User Alredy Registered
+                </div>
+            )}
             <form action="" className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-                <input type="email" placeholder="Email" value={email} onChange={ev => setEmail(ev.target.value)}/>
-                <input type="password" placeholder="Password" value={password} onChange={ev => setPassword(ev.target.value)} />
-                <button type="submit">Register</button>
+                <input type="email" placeholder="Email" value={email} disabled={CreatingUser} onChange={ev => setEmail(ev.target.value)}/>
+                <input type="password" placeholder="Password" value={password} disabled={CreatingUser} onChange={ev => setPassword(ev.target.value)} />
+                <button type="submit" disabled={CreatingUser}>Register</button>
             <div className="my-4 text-center text-gray-600">
                 Or Login 
             </div>
