2d36ddfa036eeb53b5551b360a3147798bed39a9
Md Adnan <mdadnans545@gmail.com>
2024-02-05 14:54:16 +0000

    Invoice

---
+++ src/app/pages/invoice/invoice.js
@@ -0,0 +1,22 @@
+"use client";
+function invoice(){
+return(
+    <>
+    <main>
+        <header>
+            <div>
+                <h2> Invoicer</h2>
+            </div>
+            <div>
+                <ul>
+                    <li>Print</li>
+                    <li>Download</li>
+                    <li>Send</li>
+                </ul>
+            </div>
+        </header>
+    </main>
+    </>
+);
+}
+export default invoice;
