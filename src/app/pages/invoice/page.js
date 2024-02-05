3d348aa6f207c4e97739a36da33a8967b10b38cf
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-05 14:58:43 +0000

    Error Handled

---
+++ src/app/pages/invoice/page.js
@@ -0,0 +1,21 @@
+"use client";
+export default function invoice(){
+    return(
+        <>
+        <main>
+            <header>
+                <div>
+                    <h2> Invoicer</h2>
+                </div>
+                <div>
+                    <ul>
+                        <li>Print</li>
+                        <li>Download</li>
+                        <li>Send</li>
+                    </ul>
+                </div>
+            </header>
+        </main>
+        </>
+    );
+}
