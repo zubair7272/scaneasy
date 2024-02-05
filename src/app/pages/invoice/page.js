7f363f39fd5b9986f625acce2b512d95c32fd09b
Md Adnan <mdadnans545@gmail.com>
2024-02-05 15:14:06 +0000

    Invoice update

--- src/app/pages/invoice/page.js
+++ src/app/pages/invoice/page.js
@@ -3,18 +3,26 @@ export default function invoice(){
     return(
         <>
         <main>
+            {/* Header */}
             <header>
                 <div>
                     <h2> Invoicer</h2>
                 </div>
                 <div>
                     <ul>
-                        <li>Print</li>
+                        <li><></></li>
                         <li>Download</li>
                         <li>Send</li>
                     </ul>
                 </div>
             </header>
+            {/* End Of Header */}
+
+            {/* Your Details */}
+            <section>
+                <input type="text" name="text" id="text" placeholder="Enter your name" required />
+            </section>
+            {/* End of Your Details */}
         </main>
         </>
     );
