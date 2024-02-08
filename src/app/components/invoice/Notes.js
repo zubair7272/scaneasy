8ffa4f254cbe363f0b86b15415ec7600c565f3a3
Md Adnan <mdadnans545@gmail.com>
2024-02-08 21:48:01 +0000

    Created Components of all the sections of Invoice main page(/invoice/pages.js)

---
+++ src/app/components/invoice/Notes.js
@@ -0,0 +1,12 @@
+export default function Notes(){
+    return(
+        <>
+        <section className="mb-5">
+                {/* Textarea */}
+                <p>
+                    Notes to Clients....
+                </p>
+            </section>             
+        </>
+    );
+}
---
+++ src/app/components/invoice/YourDetails.js
@@ -0,0 +1,10 @@
+export default function YourDetails(){
+    return(
+        <>
+            <section className="flex flex-col items-end justify-end">
+                <h2 className="text-xl uppercase">Mohammad Adnan</h2>
+                <p>Your Address</p>
+            </section>
+        </>
+    );
+}
