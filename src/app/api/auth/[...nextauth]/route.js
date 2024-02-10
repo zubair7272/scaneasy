2d5474e8b0ee51abdc6781fd77ecb75f03ffab59
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-10 12:07:20 +0000

    Added Google Authentication

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,10 +3,15 @@ import NextAuth from "next-auth"
 import { User } from "../../../models/User"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt"
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     secret: process.env.SECRET,
     providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
         CredentialsProvider({
           name: 'credentials',
           id : 'credentials',
