3e51519ca372806ea8b9b821e7cdc658f4dfe8de
Md Adnan <mdadnans545@gmail.com>
2024-02-17 12:07:57 +0000

    customer details

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 // import ContactForm from "./components/layouts/ContactForm";
 import Header  from "./components/layouts/Header";
 import Hero from "./components/layouts/Hero";
-// import Menu from "./components/layouts/Menu";
-// import Items  from "./components/layouts/Items";
+//  import Menu from "../components/layouts/Menu";
+// import Items  from "../components/layouts/Items";
 export default function Home() {
   return (
     <>
       {/* <Header /> */}
       <Hero />
-      {/* <Items /> */}
-      {/* <Menu /> */}
+      {/* <Items />
+      <Menu /> */}
     </>
   )
 }
