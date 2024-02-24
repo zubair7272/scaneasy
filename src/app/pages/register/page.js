2d6c3e53b4c71fda11b9081b76901077e65c90a5
Mohammed Zubair <nisarzubair414@gmail.com>
2024-02-24 08:33:00 +0000

    prod debug

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -4,7 +4,6 @@
 import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 // import { json } from "stream/consumers";
 export default function Register(){
     const [email,setEmail] = useState('')
