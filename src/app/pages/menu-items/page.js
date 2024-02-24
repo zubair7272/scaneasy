23ec34af2501191db0767f5146b7bc4a2fb2b68e
adnan545 <mdadnans545@gmial.com>
2024-02-24 19:20:32 +0530

    menu-items

--- src/app/pages/menu-items/page.js
+++ src/app/pages/menu-items/page.js
@@ -1,5 +1,18 @@
-export default function MenuItemsPage{
-    <section>
-        <UserTabs />
-    </section>
+'use client';
+import UserTabs from "@components/layout/UserTabs";
+import {useProfile} from "@components/UserProfile";
+ export default function MenuItemsPage(){
+    const {loading, data} = useProfile();
+    if (loading){
+        return 'Loading user info...';
+    }
+
+    if(!data.admin){
+        return 'Not an admin.';
+    }
+    return(
+        <section className="mt-8">
+             <UserTabs isAdmin={true} />
+         </section>
+         );
 }
