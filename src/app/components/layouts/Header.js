3a7f9778a8750ad26de4b311b2f5836c6ea7b156
adnan545 <mdadnans545@gmial.com>
2024-03-03 14:02:57 +0530

    Order

--- src/app/components/layouts/Header.js
+++ src/app/components/layouts/Header.js
@@ -1,49 +1,103 @@
 'use client';
-import { signOut, useSession } from "next-auth/react";
-import Link  from "next/link";
-import Image from "next/image";
+import {CartContext} from "../components/AppContext";
+import Bars2 from "../icons/Bars2";
+import ShoppingCart from "../icons/ShoppingCart";
+import {signOut, useSession} from "next-auth/react";
+import Link from "next/link";
+import {useContext, useState} from "react";
+
+function AuthLinks({status, userName}) {
+  if (status === 'authenticated') {
+    return (
+      <>
+        <Link href={'/profile'} className="whitespace-nowrap">
+          Hello, {userName}
+        </Link>
+        <button
+          onClick={() => signOut()}
+          className="bg-primary rounded-full text-white px-8 py-2">
+          Logout
+        </button>
+      </>
+    );
+  }
+  if (status === 'unauthenticated') {
+    return (
+      <>
+        <Link href={'/login'}>Login</Link>
+        <Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">
+          Register
+        </Link>
+      </>
+    );
+  }
+}
+
 export default function Header() {
   const session = useSession();
-
-
-  // console.log(session)
-  const status  = session?.status
+  const status = session?.status;
   const userData = session.data?.user;
-  let userName = userData?.name || userData?.email
-  if(userName && userName.includes(' ')){
-    userName = userName.split(' ')[0]
+  let userName = userData?.name || userData?.email;
+  const {cartProducts} = useContext(CartContext);
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  if (userName && userName.includes(' ')) {
+    userName = userName.split(' ')[0];
   }
-  if(userName && userName.includes('@')){
-    userName = userName.split('@')[0]
-  }
-    return(
-        <header className="my-6 flex items-center justify-between">
-        
-        <Link className="ml-5" href=""><Image src={'/Logo.jpg'} alt="Background" width={120} height={80} /></Link>
-
-        <nav className="flex  items-center gap-8 text-gray-400 font-semibold">
-          <Link href="">Home</Link>
-          <Link href="">Menu</Link>
-          <Link href="">About</Link>
-          <Link href="">Contact</Link>
+  return (
+    <header>
+      <div className="flex items-center md:hidden justify-between">
+        <Link className="text-primary font-semibold text-2xl" href={'/'}>
+          ST PIZZA
+        </Link>
+        <div className="flex gap-8 items-center">
+          <Link href={'/cart'} className="relative">
+            <ShoppingCart />
+            {cartProducts?.length > 0 && (
+              <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
+            {cartProducts.length}
+          </span>
+            )}
+          </Link>
+          <button
+            className="p-1 border"
+            onClick={() => setMobileNavOpen(prev => !prev)}>
+            <Bars2 />
+          </button>
+        </div>
+      </div>
+      {mobileNavOpen && (
+        <div
+          onClick={() => setMobileNavOpen(false)}
+          className="md:hidden p-4 bg-gray-200 rounded-lg mt-2 flex flex-col gap-2 text-center">
+          <Link href={'/'}>Home</Link>
+          <Link href={'/menu'}>Menu</Link>
+          <Link href={'/#about'}>About</Link>
+          <Link href={'/#contact'}>Contact</Link>
+          <AuthLinks status={status} userName={userName} />
+        </div>
+      )}
+      <div className="hidden md:flex items-center justify-between">
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link className="text-primary font-semibold text-2xl" href={'/'}>
+            ST PIZZA
+          </Link>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/menu'}>Menu</Link>
+          <Link href={'/#about'}>About</Link>
+          <Link href={'/#contact'}>Contact</Link>
         </nav>
-        <nav className="flex  items-center gap-8 text-gray-400 font-semibold">
-          {status === 'authenticated' && (
-            <>
-            <Link href={'/pages/Profile'}>Hello,{userName}</Link>
-            <button onClick={()=> signOut()} className="bg-blue-700 rounded-full text-white px-6 py-2">
-              Logout
-            </button>
-            </>
-
-          ) }
-          {status === 'unauthenticated'&&(
-            <>
-              <Link href={'/pages/login'}>Login</Link>
-              <Link href={'/pages/register'} className="bg-blue-700 rounded-full text-white px-6 py-2 mr-5">Register</Link>
-            </>
-          )}
+        <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+          <AuthLinks status={status} userName={userName} />
+          <Link href={'/cart'} className="relative">
+            <ShoppingCart />
+            {cartProducts?.length > 0 && (
+              <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
+            {cartProducts.length}
+          </span>
+            )}
+          </Link>
         </nav>
-        </header>
-    );
+      </div>
+    </header>
+  );
 }
