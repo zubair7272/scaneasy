550eb2c7b449f217583f415e1f944250e33394dd
Mohammed Zubair <nisarzubair414@gmail.com>
2024-03-07 20:16:00 +0000

    update

--- src/app/components/menu/MenuItemTile.js
+++ src/app/components/menu/MenuItemTile.js
@@ -1,5 +1,5 @@
 import AddToCartButton from "./AddToCartButton";
-
+import Image from "next/image";
 export default function MenuItemTile({onAddToCart, ...item}) {
   const {image, description, name, basePrice,
     sizes, extraIngredientPrices,
@@ -9,7 +9,7 @@ export default function MenuItemTile({onAddToCart, ...item}) {
     <div className="bg-gray-200 p-4 rounded-lg text-center
       group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
       <div className="text-center">
-        <img src={image} className="max-h-auto max-h-24 block mx-auto" alt="pizza"/>
+        <Image src={image} className="max-h-auto max-h-24 block mx-auto" alt="pizza" />
       </div>
       <h4 className="font-semibold text-xl my-3">{name}</h4>
       <p className="text-gray-500 text-sm line-clamp-3">
