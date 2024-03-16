2d304e2eaeb9597156be347a0ac3fcfc0f8553fb
zubair7272 <64643003+zubair7272@users.noreply.github.com>
2024-03-17 05:11:59 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,17 @@ const nextConfig = {
         ]
 
     }
+        eslint:{
+
+            ignoreDuringBuilds : true
+        },
+        typescript: {
+            // !! WARN !!
+            // Dangerously allow production builds to successfully complete even if
+            // your project has type errors.
+            // !! WARN !!
+            ignoreBuildErrors: true,
+        },
 };
 
 export default nextConfig;
