16e93c9d78290bcbd9980126521058b59e52eeab
Mohammed Zubair <nisarzubair414@gmail.com>
2024-03-24 10:45:27 +0000

    currency update

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -44,7 +44,7 @@ export async function POST(req) {
     stripeLineItems.push({
       quantity: 1,
       price_data: {
-        currency: 'USD',
+        currency: 'SAR',
         product_data: {
           name: productName,
         },
@@ -68,7 +68,7 @@ export async function POST(req) {
         shipping_rate_data: {
           display_name: 'Other Charges',
           type: 'fixed_amount',
-          fixed_amount: {amount: 0, currency: 'USD'},
+          fixed_amount: {amount: 0, currency: 'SAR'},
         },
       }
     ],
