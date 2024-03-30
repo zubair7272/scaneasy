9641ea25ef15e529aff6644eedeab19da0715cac
Mohammed Zubair <nisarzubair414@gmail.com>
2024-03-30 10:14:30 +0000

    prod error

--- src/app/api/pusher-auth/route.js
+++
@@ -1,9 +0,0 @@
-// pages/api/pusher-auth.js
-import { pusherServer } from "../../libs/pusher";
-
-export default (req, res) => {
-  const socketId = req.body.socket_id;
-  const channel = req.body.channel_name;
-  const auth = pusherServer.authenticate(socketId, channel);
-  res.send(auth);
-};
